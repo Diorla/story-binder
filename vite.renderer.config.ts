526ca080c6c22c08886d387ccdc117df957abe20
Ade Adeola <adedotster@gmail.com>
2024-08-24 14:56:17 +0100

    husky added

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,7 @@
 import type { ConfigEnv, UserConfig } from "vite";
 import { defineConfig } from "vite";
 import { pluginExposeRenderer } from "./vite.base.config";
-import path from "path";
+
 // https://vitejs.dev/config
 export default defineConfig((env) => {
   const forgeEnv = env as ConfigEnv<"renderer">;
