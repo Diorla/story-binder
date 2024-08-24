bfef2942b9ab21eaba12fab6078076619ec0255c
Ade Adeola <adedotster@gmail.com>
2024-08-24 12:38:31 +0100

    add base url import to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import * as React from "react";
 import { createRoot } from "react-dom/client";
+import Temp from "./containers/Temp";
 
 const root = createRoot(document.getElementById("app"));
-root.render(<h2>Hello from React!</h2>);
+root.render(
+  <div>
+    <h2>Hello from React!</h2>
+    <Temp />
+  </div>
+);
