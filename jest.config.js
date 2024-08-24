048e36da3bd6ed438004bf4dbb6c28450f1099b6
Ade Adeola <adedotster@gmail.com>
2024-08-24 13:18:50 +0100

    add test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)": "<rootDir>/src/$1",
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx,js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/story-binder.d.ts",
+    "!**/*.config.ts",
+    "!**/*.config.js",
+    "!**/.vite/**",
+    "!**/*.d.ts",
+  ],
 };
