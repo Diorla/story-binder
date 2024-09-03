953f60f4a7fd333fa47db2f3abdd4000e07023ca
Ade Adeola <adedotster@gmail.com>
2024-09-03 23:02:15 +0100

    project list validation

--- src/containers/home/getProjectList.tsx
+++ src/containers/home/getProjectList.tsx
@@ -1,6 +1,23 @@
 import logError from "@/scripts/logError";
 import Directory from "@/types/Directory";
 import ProjectInfo from "@/types/ProjectInfo";
+import Ajv, { JSONSchemaType } from "ajv";
+
+const ajv = new Ajv();
+
+const schema: JSONSchemaType<ProjectInfo> = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+    summary: { type: "string" },
+    cover: { type: "string" },
+    path: { type: "string" },
+  },
+  required: ["id", "name", "summary", "cover", "path"],
+};
+
+const validate = ajv.compile<ProjectInfo>(schema);
 
 export default async function getProjectList(workspace: string) {
   const dirNames = (await window.api?.sendMessage({
@@ -18,7 +35,8 @@ export default async function getProjectList(workspace: string) {
         path,
       })) as ProjectInfo;
 
-      list.push({ ...res, path: `${workspace}/${project}` });
+      const value = { ...res, path: `${workspace}/${project}` };
+      if (validate(value)) list.push(value);
     }
   } catch (error) {
     logError("Projects", "getProjectList", error);
