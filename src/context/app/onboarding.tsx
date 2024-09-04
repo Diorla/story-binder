0686a0b47b58fc75fd53c9f8dbffc8ea6489d2f5
Ade Adeola <adedotster@gmail.com>
2024-09-04 15:43:33 +0100

    onboarding images

--- src/context/app/onboarding.tsx
+++ src/context/app/onboarding.tsx
@@ -1,30 +1,40 @@
 import BROWSER from "@/constants/BROWSER";
 import useLocalState from "@/hooks/useLocalState";
-import { Button, Card, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import useApp from "./useApp";
 
+const count = 14;
 export default function Onboarding() {
   const [page, setPage] = useLocalState("onboarding-page", 1);
   const { updateUserInfo } = useApp();
-  const isCompleted = page === 10;
+  const isCompleted = page === count;
+
+  const isCompleteClass = isCompleted ? "completed" : "";
+  const className = `onboarding onboarding-${page} ${isCompleteClass}`;
 
   return (
     <Container
       maxWidth={false}
       disableGutters={true}
-      style={{ minHeight: BROWSER.HEIGHT }}
+      sx={{ minHeight: BROWSER.HEIGHT }}
     >
       <Stack style={{ minHeight: BROWSER.HEIGHT }}>
-        <Stack style={{ flex: 1 }}>
-          <Card style={{ flex: 1 }}>
-            This is where onboarding will be shown for new users {page}
-          </Card>
+        <Stack style={{ flex: 1, backgroundColor: "transparent" }}>
+          <Stack style={{ flex: 1 }} className={className}>
+            {isCompleted ? (
+              <Typography variant="h5" sx={{ textAlign: "center" }}>
+                You have completed the onboarding process
+              </Typography>
+            ) : (
+              ""
+            )}
+          </Stack>
         </Stack>
         <Stack sx={{ alignItems: "center", m: 1 }}>
           <Pagination
-            count={10}
+            count={count}
             variant="outlined"
             shape="rounded"
             onChange={(_e, page) => setPage(page)}
