7d7263b153460e54b421aca5de73debdf141bf34
Ade Adeola <adedotster@gmail.com>
2024-09-04 11:15:04 +0100

    template renderer formatted

--- src/containers/templates/index.tsx
+++ src/containers/templates/index.tsx
@@ -5,13 +5,12 @@ import Typography from "@mui/material/Typography";
 import { Box, Button, Card, Divider } from "@mui/material";
 import useRouter from "@/context/router/useRouter";
 import getTemplates from "@/services/get-templates";
-import useApp from "@/context/app/useApp";
+import EmptyTemplate from "./EmptyTemplate";
 
 export default function Templates() {
   const [loading, setLoading] = useState(true);
   const [templates, setTemplates] = useState<Template[]>([]);
   const { navigate } = useRouter();
-  const { refresh } = useApp();
 
   useEffectOnce(() => {
     getTemplates().then((list) => {
@@ -20,28 +19,14 @@ export default function Templates() {
     });
   });
 
+  const refresh = () => {
+    getTemplates().then((list) => {
+      setTemplates(list);
+    });
+  };
+
   if (loading) return <div>Loading</div>;
-  if (!templates.length)
-    return (
-      <div id="empty-template">
-        <div
-          style={{ backgroundColor: "rgba(255, 255, 255, 0.7)" }}
-          className="empty-template-wrapper"
-        >
-          <Typography variant="h5">No templates found</Typography>
-          <Typography>
-            Oops, where did all those template go? We need to create new
-            template
-          </Typography>
-          <Button
-            variant="outlined"
-            onClick={() => navigate("create-template")}
-          >
-            New template
-          </Button>
-        </div>
-      </div>
-    );
+  if (!templates.length) return <EmptyTemplate />;
   return (
     <div>
       <Box className="row" sx={{ justifyContent: "space-between" }}>
